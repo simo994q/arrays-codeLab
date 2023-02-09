552c950f437d31150649b26e3854063215192515
simo994q <simonkn1@hotmail.com>
2023-02-09 09:30:08 +0100

    2

--- assets/js/opgaver/arrayFunctions.js
+++ assets/js/opgaver/arrayFunctions.js
@@ -9,23 +9,27 @@ let myPartArray = ['partElement1', 'partElement2', 'PartElement3'];
 // opgave 1 tilføj element4 til enden myArray
 
 // din kode her...
-console.log(myArray);
+// myArray.push('element4')
+// console.log(myArray);
 
 // opgave 1 fjern element4 igen
 
 // din kode her...
-console.log(myArray);
+// myArray.pop()
+// console.log(myArray);
 
 //.........................................................
 
 // opgave 2 tilføj element4 til starten myArray
 
 // din kode her...
+myArray.unshift('element4')
 console.log(myArray);
 
 // opgave 2 fjern element4 igen
 
 // din kode her...
+myArray.shift()
 console.log(myArray);
 
 //.........................................................
@@ -39,8 +43,7 @@ let myVegetables = ['salat', 'blomkål', 'spidskål', 'agurk'];
  */
 
 // din kode her...
-
-myFruits.splice(2, 0, "mandarin", "fersken");
+myFruits.splice(2, 0, 'mandarin', 'fersken')
 
 console.log(myFruits);
 
@@ -58,6 +61,7 @@ træk blomkål og spidskål ud af arrayen myVegetables
 og ind i en ny array der hedder myCabbage*/
 
 // din kode her...
+let myCabbage = myVegetables.slice(1, 3)
 
 
 
@@ -70,8 +74,7 @@ console.log(myCabbage);
 
 
 // din kode her...
-
-
+let myYummies = myFruits.concat(myVegetables)
 
 console.log(myYummies);
 
@@ -83,7 +86,7 @@ console.log(myYummies);
 
 
 // din kode her...
-
+myYummies.sort()
 
 
 console.log(myYummies);
@@ -94,6 +97,7 @@ console.log(myYummies);
 
 
 // din kode her...
+myYummies.reverse()
 
 console.log(myYummies);
 
@@ -107,7 +111,13 @@ console.log(myYummies);
 let myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // din kode her...
+let myNextNumbers = myNumbers.map((x) => {
+    return x + 7
+})
 console.log(myNextNumbers);
+let myLastNumbers = myNextNumbers.map((x) => {
+    return x * 12
+})
 console.log(myLastNumbers);
 
 //.........................................................
@@ -116,6 +126,15 @@ skriv en kode der kan give os en ny array, udfra myNames, hvor navnet keld ikke 
 */
 let myNames = ['keld', 'søren', 'jens', 'tine', 'keld', 'ib', 'lene', 'keld', 'frederikke', 'line', 'kurt'];
 
+let noKeld = myNames.filter(checkKeld)
+
+function checkKeld (name) {
+    if (name != 'keld') {
+        return name
+    }
+
+};
+console.log(noKeld);
 
 //.........................................................
 
