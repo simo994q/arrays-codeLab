add366ca262edd534e1f559e2ed4fc055cca195c
simo994q <simonkn1@hotmail.com>
2023-02-09 12:10:00 +0100

    bruh

--- assets/js/opgaver/examples.js
+++ assets/js/opgaver/examples.js
@@ -3,28 +3,49 @@ const myExtrasInput = document.getElementById('extraUdstyr');
 const myExtrasListElement = document.getElementById('extraList');
 
 
-myExtrasInput.addEventListener("change", addExtra);
 
+myExtrasInput.addEventListener('change', addExtra);
 
 
 function addExtra() {
-    /* din map kode her... */
+    let selectedValue = myExtrasInput.value
+    myExtras.push(selectedValue)
+    updateExtras()
 }
+
 
 
 
 function updateExtras() {
-
-
-
-
-    /* din map kode her... */
-
-
+        let myExtrasLast = myExtras.slice(-1)
+        myExtrasListElement.innerHTML += `<p>${myExtrasLast}</p>`
 }
 
 
 
 
 
+// function updateExtras () {
+//     let myExtrasNew = myExtras.map( (x) => {
+//         return x 
+//     })
+// };
 
+
+// const myExtrasInput2 = document.getElementById('extraUdstyr2');
+
+// myExtrasInput2.addEventListener('change', addExtra2);
+
+
+
+// function addExtra2 () {
+//     let delSelect = myExtrasInput2.value
+//     let deletedSel = myExtras.filter(delFunc(delSelect))
+
+
+
+// };
+
+// function delFunc (delSel) {
+
+// };
